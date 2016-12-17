49d442e54db55f2bd4a7cc6124de294fb9c3ad20
Aspen Hollyer <aspen.dollins@gmail.com>
2016-12-17 09:31:27 -0600

    Add marker, eraser, rainbow, clear, and grid size functionality

--- js/sketch.js
+++ js/sketch.js
@@ -1,18 +1,66 @@
-var gridSize = 16;
-
-
-$(document).ready(function() {
-
+function drawGrid(){
 	for (var i = 0; i < gridSize * gridSize; i++) {
-		$("#canvas").append('<div class="box"></div>');
+		$("#canvas").append('<div class="box"></div>')
 	}
 
 	$(".box").css({
 		'width': $('#canvas').width() / gridSize,
 		'height': $('#canvas').height() / gridSize
 	})
-	
+}
+
+function marker() {
 	$(".box").mouseenter(function(){
 		$(this).css("background-color", "#b2b2b2")
 	})
+}
+
+function eraser() {
+	$(".box").mouseenter(function(){
+		$(this).css("background-color", "#FFF")
+	})
+}
+
+function rainbow() {
+	$(".box").mouseenter(function(){
+		var rainbowPaint = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+		$(this).css("background", rainbowPaint);
+	})
+}
+
+$(document).ready(function() {
+
+	gridSize = 16;
+
+	drawGrid();
+
+	marker();
+
+	$('[name="clear"]').click(function(){
+		$(".box").css("background-color", "#FFF")
+	})
+
+	$('[name="eraser"]').click(function(){
+		eraser();
+	})
+
+	$('[name="marker"]').click(function(){
+		marker();
+	})
+
+	$('[name="paintbrush"]').click(function(){
+		paintbrush();
+	})
+
+	$('[name="rainbow"]').click(function(){
+		rainbow();
+	})
+
+	$('[name="change"]').click(function(){
+		gridSize = prompt("Please enter a number from 2-64 to change the SketchPad's grid size. For example, '16' generates a 16x16 grid.", "16");
+		$('#canvas').empty();
+		drawGrid();
+		marker();
+	})
+
 });
