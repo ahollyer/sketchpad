38632d1450f9c7a3fe189b67389a1f32f87ac8d9
Aspen Hollyer <aspen.dollins@gmail.com>
2016-12-18 06:25:34 -0600

    Add toggle outline feature

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,7 @@
 function drawGrid(){
 	for (var i = 0; i < gridSize * gridSize; i++) {
-		$("#canvas").append('<div class="box"></div>')
+		$("#canvas").append('<div class="box outline"></div>')
 	}
-
 	$(".box").css({
 		'width': $('#canvas').width() / gridSize,
 		'height': $('#canvas').height() / gridSize
@@ -17,7 +16,7 @@ function marker() {
 
 function eraser() {
 	$(".box").mouseenter(function(){
-		$(this).css("background-color", "#FFF")
+		$(this).css("opacity", "0")
 	})
 }
 
@@ -53,10 +52,14 @@ $(document).ready(function() {
 	})
 
 	$('[name="change"]').click(function(){
-		gridSize = prompt("Please enter a number from 2-64 to change the SketchPad's grid size. For example, '16' generates a 16x16 grid.", "16");
+		gridSize = prompt("Please enter a number from 2-40 to change the SketchPad's grid size. For example, '16' generates a 16x16 grid.", "16");
 		$('#canvas').empty();
 		drawGrid();
 		marker();
+	})
+
+	$('[name="toggle"]').click(function() {
+		$(".box").toggleClass("outline");
 	})
 
 });
